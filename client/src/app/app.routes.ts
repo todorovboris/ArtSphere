478ed0aa71b2cc1cf75eb857cfcc0bed1a6acd57
Boris Todorov <btodorovtodorov@gmail.com>
2025-08-07 18:31:56 +0300

    add routes for home, login and register

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./components/auth/register/register.component').then(
+        (c) => c.RegisterComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/auth/login/login.component').then(
+        (c) => c.LoginComponent
+      ),
+  },
+];
