2761d7841d85faf097a305ee8343df82679d4063
Boris Todorov <btodorovtodorov@gmail.com>
2025-08-07 20:29:28 +0300

    login component logic create

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
+  // {
+  //   path: '',
+  //   redirectTo: '/home',
+  //   pathMatch: 'full',
+  // },
   {
     path: 'register',
     loadComponent: () =>
