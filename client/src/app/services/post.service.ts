a906568761dc703f33eec8d41064d018e2cf3550
Boris Todorov <btodorovtodorov@gmail.com>
2025-08-11 19:28:45 +0300

    Create post.service.ts

---
+++ client/src/app/services/post.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  private apiUrl = 'http://localhost:3000/api/posts';
+
+  constructor(private http: HttpClient) {}
+
+  getAllPosts(limit?: number): Observable<Post[]> {
+    if (limit) {
+      this.apiUrl += `?limit=${limit}`;
+    }
+
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+
+  createPost(themeName: string, postText: string): Observable<Post> {
+    const body = JSON.stringify({ themeName, postText });
+    return this.http.post<Post>(this.apiUrl, body, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
