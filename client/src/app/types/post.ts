3eaf237456be5c9b501e8536e6e8e1ac54aa48c4
Boris Todorov <btodorovtodorov@gmail.com>
2025-08-13 09:30:51 +0300

    update post type with adding imageUrl, title and author

--- client/src/app/types/post.ts
+++ client/src/app/types/post.ts
@@ -3,6 +3,9 @@ import { User } from './user';
 export interface Post {
   _id: string;
   userId: User;
+  imageUrl: string;
+  title: string;
+  author: string;
   createdAt: string;
   updatedAt: string;
   likes: string[];
